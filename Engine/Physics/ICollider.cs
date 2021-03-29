ddc70a0d26aaa7244545d77a10794ad77311834f
amadornes <amadornes@gmail.com>
2021-03-29 18:30:33 +0200

    Add empty colliders

--- Engine/Physics/ICollider.cs
+++ Engine/Physics/ICollider.cs
@@ -5,6 +5,17 @@ namespace DigBuild.Engine.Physics
 {
     public interface ICollider
     {
+        static ICollider None { get; } = new NoCollider();
+
         bool Collide(AABB target, Vector3 motion, out Vector3 intersection);
+
+        private sealed class NoCollider : ICollider
+        {
+            public bool Collide(AABB target, Vector3 motion, out Vector3 intersection)
+            {
+                intersection = Vector3.Zero;
+                return false;
+            }
+        }
     }
 }
--- Engine/Physics/IRayCollider.cs
+++ Engine/Physics/IRayCollider.cs
@@ -4,6 +4,17 @@ namespace DigBuild.Engine.Physics
 {
     public interface IRayCollider<THit>
     {
+        static IRayCollider<THit> None { get; } = new NoCollider();
+
         bool TryCollide(Raycast.Ray ray, [NotNullWhen(true)] out THit? hit);
+
+        private sealed class NoCollider : IRayCollider<THit>
+        {
+            public bool TryCollide(Raycast.Ray ray, [NotNullWhen(true)] out THit? hit)
+            {
+                hit = default;
+                return false;
+            }
+        }
     }
 }

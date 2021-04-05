dfed5bd506d77f23cac4e07b133623b60062e4f8
amadornes <amadornes@gmail.com>
2021-04-05 17:13:35 +0200

    Add read-only block context

---
+++ Engine/Blocks/ReadOnlyBlockContext.cs
@@ -0,0 +1,19 @@
+﻿using DigBuild.Engine.Math;
+using DigBuild.Engine.Worlds;
+
+namespace DigBuild.Engine.Blocks
+{
+    public sealed class ReadOnlyBlockContext : IReadOnlyBlockContext
+    {
+        public IReadOnlyWorld World { get; }
+        public BlockPos Pos { get; }
+        public Block Block { get; }
+
+        public ReadOnlyBlockContext(IReadOnlyWorld world, BlockPos pos, Block block)
+        {
+            World = world;
+            Pos = pos;
+            Block = block;
+        }
+    }
+}

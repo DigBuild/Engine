4f2420f7b92ea0bdad0949a46d5deabda9ba81fc
amadornes <amadornes@gmail.com>
2021-04-07 11:59:04 +0200

    Add nullable and simple serdes

---
+++ Engine/Serialization/NullableSerdes.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace DigBuild.Engine.Serialization
+{
+    public sealed class NullableSerdes<T> : ISerdes<T?> where T : class
+    {
+        private readonly ISerdes<T> _serdes;
+
+        internal NullableSerdes(ISerdes<T> serdes)
+        {
+            _serdes = serdes;
+        }
+
+        public void Serialize(Stream stream, T? obj)
+        {
+            stream.WriteByte((byte) (obj != null ? 1 : 0));
+            if (obj != null)
+                _serdes.Serialize(stream, obj);
+        }
+
+        public T? Deserialize(Stream stream)
+        {
+            if (stream.ReadByte() > 0)
+                return _serdes.Deserialize(stream);
+            return null;
+        }
+    }
+
+    public static class NullableSerdesExtensions
+    {
+        public static ISerdes<T?> Nullable<T>(this ISerdes<T> serdes) where T : class
+        {
+            return new NullableSerdes<T>(serdes);
+        }
+    }
+}
---
+++ Engine/Serialization/SimpleSerdes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace DigBuild.Engine.Serialization
+{
+    public sealed class SimpleSerdes<T> : ISerdes<T>
+    {
+        private readonly Action<Stream, T> _serialize;
+        private readonly Func<Stream, T> _deserialize;
+
+        public SimpleSerdes(Action<Stream, T> serialize, Func<Stream, T> deserialize)
+        {
+            _serialize = serialize;
+            _deserialize = deserialize;
+        }
+
+        public void Serialize(Stream stream, T obj)
+        {
+            _serialize(stream, obj);
+        }
+
+        public T Deserialize(Stream stream)
+        {
+            return _deserialize(stream);
+        }
+    }
+}

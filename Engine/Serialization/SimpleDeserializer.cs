6251f19000624d64e38184a8c0322755e225c07b
amadornes <amadornes@gmail.com>
2021-04-07 18:30:04 +0200

    Add simple deserializer

---
+++ Engine/Serialization/SimpleDeserializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace DigBuild.Engine.Serialization
+{
+    public sealed class SimpleDeserializer<T> : IDeserializer<T>
+    {
+        private readonly Func<Stream, T> _deserialize;
+
+        public SimpleDeserializer(Func<Stream, T> deserialize)
+        {
+            _deserialize = deserialize;
+        }
+        
+        public T Deserialize(Stream stream)
+        {
+            return _deserialize(stream);
+        }
+    }
+}

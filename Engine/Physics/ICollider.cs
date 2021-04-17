698a147a75f0102b86e9c789a20f92ef1cea4723
amadornes <amadornes@gmail.com>
2021-04-17 08:47:45 +0200

    Add point colliders

--- Engine/Physics/ICollider.cs
+++ Engine/Physics/ICollider.cs
@@ -7,10 +7,18 @@ namespace DigBuild.Engine.Physics
     {
         static ICollider None { get; } = new NoCollider();
 
+        bool Collide(Vector3 target, Vector3 motion, out Vector3 intersection);
+
         bool Collide(AABB target, Vector3 motion, out Vector3 intersection);
 
         private sealed class NoCollider : ICollider
         {
+            public bool Collide(Vector3 target, Vector3 motion, out Vector3 intersection)
+            {
+                intersection = Vector3.Zero;
+                return false;
+            }
+
             public bool Collide(AABB target, Vector3 motion, out Vector3 intersection)
             {
                 intersection = Vector3.Zero;

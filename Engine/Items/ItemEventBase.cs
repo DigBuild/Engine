c5e624ab6fadb918312b67f4990d3c91dee89927
amadornes <amadornes@gmail.com>
2021-04-22 02:47:15 +0200

    Missed ItemEventBase

---
+++ Engine/Items/ItemEventBase.cs
@@ -0,0 +1,12 @@
+﻿namespace DigBuild.Engine.Items
+{
+    public abstract class ItemEventBase : IItemEvent
+    {
+        public ItemInstance Item { get; }
+
+        protected ItemEventBase(ItemInstance item)
+        {
+            Item = item;
+        }
+    }
+}

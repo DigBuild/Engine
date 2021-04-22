c90aaef28c66a7a9d9d5f4fdf7c6ded7477be10b
amadornes <amadornes@gmail.com>
2021-04-22 10:14:14 +0200

    Add event bus

---
+++ Engine/Events/EventBus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace DigBuild.Engine.Events
+{
+    public sealed class EventBus
+    {
+        public void Subscribe<TEvent>(Action<TEvent> handler) where TEvent : IEvent
+        {
+            var subscriptionSet = SubscriptionSet<TEvent>.Get(this);
+            subscriptionSet.Event += handler;
+        }
+
+        public void Unsubscribe<TEvent>(Action<TEvent> handler) where TEvent : IEvent
+        {
+            var subscriptionSet = SubscriptionSet<TEvent>.Get(this);
+            subscriptionSet.Event -= handler;
+        }
+
+        public void Post<TEvent>(TEvent evt) where TEvent : IEvent
+        {
+            var subscriptionSet = SubscriptionSet<TEvent>.Get(this);
+            subscriptionSet.Post(evt);
+        }
+        
+        private sealed class SubscriptionSet<TEvent> where TEvent : IEvent
+        {
+            private static ConditionalWeakTable<EventBus, SubscriptionSet<TEvent>> Store { get; } = new();
+            public static SubscriptionSet<TEvent> Get(EventBus bus) => Store.GetOrCreateValue(bus);
+
+            public event Action<TEvent>? Event;
+
+            public void Post(TEvent evt)
+            {
+                Event?.Invoke(evt);
+            }
+        }
+    }
+}

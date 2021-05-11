ae0ab75e159acce1804d745ef293e2687143eb91
amadornes <amadornes@gmail.com>
2021-05-11 17:11:53 +0200

    Add wind support to particle systems

--- Engine/Particles/ParticleUpdateContext.cs
+++ Engine/Particles/ParticleUpdateContext.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Numerics;
 
 namespace DigBuild.Engine.Particles
 {
     public interface IParticleUpdateContext
     {
         Random Random { get; }
+
+        Vector3 GetWindAt(Vector3 position);
     }
 
     public sealed class ParticleUpdateContext : IParticleUpdateContext
     {
-        public Random Random { get; } = new Random();
+        public Random Random { get; } = new();
+
+        public Vector3 Wind { get; set; }
+
+        public Vector3 GetWindAt(Vector3 position) => Wind;
     }
 }

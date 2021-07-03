d254b3e979dbcaf5f8342e65f32235f85e2790bd
amadornes <amadornes@gmail.com>
2021-07-03 18:41:28 +0200

    Extend texture stitching support to support any square 2^n textures

--- Engine/Textures/TextureStitcher.cs
+++ Engine/Textures/TextureStitcher.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Linq;
 using System.Numerics;
 using DigBuild.Platform.Resource;
 
@@ -9,8 +10,6 @@ namespace DigBuild.Engine.Textures
 {
     public sealed class TextureStitcher
     {
-        private const int SpriteSize = 16;
-
         private readonly Dictionary<ResourceName, SpriteInfo> _sprites = new();
 
         public ISprite Add(IResource resource)
@@ -23,7 +22,15 @@ namespace DigBuild.Engine.Textures
             if (_sprites.TryGetValue(texture.Name, out var currentInfo))
                 return currentInfo.Sprite;
 
-            var info = _sprites[texture.Name] = new SpriteInfo(texture);
+            var bmp = texture.Bitmap;
+            if (bmp.Width != bmp.Height)
+                throw new ArgumentException("The sprite must be square.", nameof(texture));
+
+            var setBits = BitOperations.PopCount((uint)bmp.Width);
+            if (setBits != 1)
+                throw new ArgumentException("The sprite size must be a power of 2.", nameof(texture));
+
+            var info = _sprites[texture.Name] = new SpriteInfo(bmp);
             return info.Sprite;
         }
 
@@ -31,45 +38,98 @@ namespace DigBuild.Engine.Textures
         {
             if (_sprites.Count == 0)
                 throw new Exception("Cannot build spritesheet without any sprites.");
-            
-            var tiles = (int) System.Math.Pow(2, System.Math.Ceiling(System.Math.Log2(_sprites.Count)));
-            var size = SpriteSize * tiles;
-            var relativeSpriteSize = Vector2.One / tiles;
 
-            var bitmap = new Bitmap(size, size, PixelFormat.Format32bppArgb);
-            using var graphics = Graphics.FromImage(bitmap);
+            var bitmaps = 
+                _sprites.Values
+                    .OrderByDescending(info => info.Bitmap.Width * info.Bitmap.Height)
+                    .ToList();
 
-            var sprites = new List<ISprite>(_sprites.Count);
+            var bitmap = (Bitmap?)null;
+            var graphics = (Graphics?)null;
+            var emptyRegions = new Stack<Rectangle>();
+            var sprites = new List<Sprite>(_sprites.Count);
 
-            int x = 0, y = 0;
-            foreach (var info in _sprites.Values)
+            foreach (var info in bitmaps)
             {
-                info.Sprite.UV = new Vector2(x, y) / tiles;
-                info.Sprite.Size = relativeSpriteSize;
+                var bmp = info.Bitmap;
 
+                // If this is the first texture, just set it as our bitmap and continue
+                if (bitmap == null)
+                {
+                    bitmap = bmp;
+
+                    info.Sprite.UV = Vector2.Zero;
+                    info.Sprite.Size = Vector2.One;
+                    sprites.Add(info.Sprite);
+
+                    continue;
+                }
+
+                // Upscale if there is no room
+                if (!emptyRegions.TryPop(out var region))
+                {
+                    graphics?.Dispose();
+
+                    var oldSize = bitmap.Width;
+                    var size = oldSize * 2;
+                    var newBitmap = new Bitmap(size, size, PixelFormat.Format32bppArgb);
+                    graphics = Graphics.FromImage(newBitmap);
+
+                    // Re-draw current textures
+                    graphics.DrawImageUnscaledAndClipped(bitmap, new Rectangle(0, 0, oldSize, oldSize));
+                    
+                    // Deal with the new regions
+                    emptyRegions.Push(new Rectangle(oldSize, oldSize, oldSize, oldSize));
+                    emptyRegions.Push(new Rectangle(0, oldSize, oldSize, oldSize));
+                    region = new Rectangle(oldSize, 0, oldSize, oldSize);
+
+                    // Scale all the existing UVs
+                    foreach (var sprite in sprites)
+                    {
+                        sprite.UV /= 2;
+                        sprite.Size /= 2;
+                    }
+
+                    // Update the bitmap
+                    bitmap = newBitmap;
+                }
+
+                // Draw the texture to the region and add the sprite
+                var bmpSize = bmp.Width;
+                graphics!.DrawImageUnscaledAndClipped(bmp, new Rectangle(region.X, region.Y, bmpSize, bmpSize));
+                
+                info.Sprite.UV = new Vector2(region.X / (float) bitmap.Width, region.Y / (float) bitmap.Height);
+                info.Sprite.Size = new Vector2(bmpSize / (float) bitmap.Width, bmpSize / (float) bitmap.Height);
                 sprites.Add(info.Sprite);
 
-                graphics.DrawImageUnscaledAndClipped(info.Texture.Bitmap, new Rectangle(x * SpriteSize, y * SpriteSize, SpriteSize, SpriteSize));
+                // Subdivide if our current texture is smaller than the region we're drawing to
+                var subdiv = region.Width / bmpSize;
+                if (subdiv <= 1)
+                    continue;
 
-                x = (x + 1) % tiles;
-                y += x == 0 ? 1 : 0;
+                for (var y = subdiv - 1; y >= 0; y--)
+                for (var x = subdiv - 1; x >= 0; x--)
+                {
+                    if (x == 0 && y == 0) continue;
+                    emptyRegions.Push(new Rectangle(region.X + x * bmpSize, region.Y + y * bmpSize, bmpSize, bmpSize));
+                }
             }
-
+            
             if (outputFile != null)
-                bitmap.Save(outputFile);
-
-            return new SpriteSheet(name, bitmap, sprites);
+                bitmap!.Save(outputFile);
+            
+            return new SpriteSheet(name, bitmap!, sprites);
         }
 
         private sealed class SpriteInfo
         {
-            internal readonly IBitmapTexture Texture;
+            internal readonly Bitmap Bitmap;
 
             internal readonly Sprite Sprite = new();
 
-            public SpriteInfo(IBitmapTexture texture)
+            public SpriteInfo(Bitmap bitmap)
             {
-                Texture = texture;
+                Bitmap = bitmap;
             }
         }
 

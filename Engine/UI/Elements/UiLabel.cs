ff98b913d958759d45cd8ed4fbd20684fc12d795
amadornes <amadornes@gmail.com>
2021-08-10 17:30:49 +0200

    Allow UI labels to be yellow

--- Engine/UI/Elements/UiLabel.cs
+++ Engine/UI/Elements/UiLabel.cs
@@ -6,17 +6,19 @@ namespace DigBuild.Engine.Ui.Elements
     public sealed class UiLabel : IUiElement
     {
         private readonly TextRenderer _textRenderer;
+        private readonly bool _yellow;
         public string Text { get; set; }
 
-        public UiLabel(string text, TextRenderer textRenderer = null!)
+        public UiLabel(string text = "", TextRenderer textRenderer = null!, bool yellow = false)
         {
             _textRenderer = textRenderer ?? IUiElement.GlobalTextRenderer;
+            _yellow = yellow;
             Text = text;
         }
 
         public void Draw(RenderContext context, IGeometryBuffer buffer, float partialTick)
         {
-            _textRenderer.DrawLine(buffer, Text, 3);
+            _textRenderer.DrawLine(buffer, Text, 3, _yellow);
         }
     }
 }
